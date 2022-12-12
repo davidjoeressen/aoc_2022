5ca6eaf0e8b5c765704d128e7c9f8f0f1e6769a9
David Joeressen <david@joeressen.net>
2022-12-12 10:22:06 +0900

    add day 9 part 2

--- day_09/src/lib.rs
+++ day_09/src/lib.rs
@@ -13,14 +13,14 @@ pub type Instruction = (Direction, i32);
 #[derive(Debug)]
 struct Rope {
     head: (i32, i32),
-    tail: (i32, i32),
+    tails: Vec<(i32, i32)>,
 }
 
 impl Rope {
-    fn new() -> Rope {
+    fn new(num_tails: usize) -> Rope {
         Rope {
             head: (0, 0),
-            tail: (0, 0),
+            tails: vec![(0, 0); num_tails],
         }
     }
 
@@ -35,13 +35,18 @@ impl Rope {
     }
 
     fn update_tail(&mut self) {
-        let x = self.head.0 - self.tail.0;
-        let y = self.head.1 - self.tail.1;
-        if x == x.signum() && y == y.signum() {
-            return;
+        let mut previous = self.head;
+        for tail in self.tails.iter_mut() {
+            let x = previous.0 - tail.0;
+            let y = previous.1 - tail.1;
+            if x == x.signum() && y == y.signum() {
+                previous = *tail;
+                continue;
+            }
+            tail.0 += x.signum();
+            tail.1 += y.signum();
+            previous = *tail;
         }
-        self.tail.0 += x.signum();
-        self.tail.1 += y.signum();
     }
 }
 
@@ -63,19 +68,27 @@ pub fn parse(file: &str) -> Vec<Instruction> {
 }
 
 pub fn part1(instructions: &[Instruction]) -> i32 {
-    let mut rope = Rope::new();
+    let mut rope = Rope::new(1);
     let mut tail_positions = HashSet::new();
     for (dir, n) in instructions {
         for _ in 0..*n {
             rope.move_one(dir);
-            tail_positions.insert(rope.tail);
+            tail_positions.insert(*rope.tails.last().unwrap());
         }
     }
     tail_positions.len() as i32
 }
 
-pub fn part2(_instructions: &[Instruction]) -> i32 {
-    0
+pub fn part2(instructions: &[Instruction]) -> i32 {
+    let mut rope = Rope::new(9);
+    let mut tail_positions = HashSet::new();
+    for (dir, n) in instructions {
+        for _ in 0..*n {
+            rope.move_one(dir);
+            tail_positions.insert(*rope.tails.last().unwrap());
+        }
+    }
+    tail_positions.len() as i32
 }
 
 #[cfg(test)]
@@ -118,5 +131,20 @@ mod tests {
             (Direction::Right, 2),
         ];
         assert_eq!(part1(&have), 13);
+    }
+
+    #[test]
+    fn test_part2() {
+        let have = vec![
+            (Direction::Right, 5),
+            (Direction::Up, 8),
+            (Direction::Left, 8),
+            (Direction::Down, 3),
+            (Direction::Right, 17),
+            (Direction::Down, 10),
+            (Direction::Left, 25),
+            (Direction::Up, 20),
+        ];
+        assert_eq!(part2(&have), 36);
     }
 }
